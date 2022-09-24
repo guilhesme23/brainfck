9093698b6745929264db1ef67c571269aae1683a
Guilherme Marques <marquesguilherme3@gmail.com>
2022-09-24 19:34:31 -0300

    Implement loops

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { KeyObjectType } from "crypto"
+
 function changeValue(value: number, limit: number): number {
   let result = value%limit
   if (result<0) {
@@ -6,13 +8,43 @@ function changeValue(value: number, limit: number): number {
     return result
   }
 }
+interface IHash {
+  [index: string]: number
+}
+
+function tokenize(data: string): IHash {
+  let loopStack: number[] = [];
+  let table: IHash = {}
+  for (let i = 0; i < data.length; i++) {
+    let c = data[i]
+    switch (c) {
+      case '[':
+        loopStack.push(i)
+        break;
+      case ']':
+        let opening = loopStack.pop();
+        if (opening === undefined) {
+          throw new SyntaxError("Unexpected token: ']'");
+        } else {
+          table[opening] = i;
+          table[i] = opening;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  return table
+}
 
 function evaluate(data: string) {
-  const tapeSize = 10
+  const tapeSize = 1000
   const limit = 256
   const tape = new Array(tapeSize).fill(0)
+  let loopTable = tokenize(data)
   let cursor = 0
-  for (const c of data) {
+  for (let i=0; i<data.length; i++) {
+    let c = data[i]
     switch (c) {
       case '>':
         cursor = changeValue(cursor+1, tapeSize)
@@ -25,13 +57,24 @@ function evaluate(data: string) {
         break;
       case '-':
         tape[cursor] = changeValue(tape[cursor]-1, limit)
+        break;
+      case '[':
+        if (tape[cursor] === 0) {
+          i = loopTable[i]
+        }
+        break;
+      case ']':
+        if (tape[cursor] > 0) {
+          i = loopTable[i]
+        }
+        break;
       default:
         break;
     }
-  }
 
+  }
   console.log(tape)
 }
 
 
-evaluate("+++>>+++>>+++++--<++>>--isso aqui ele não conta");
+evaluate("++++++++++[>++++++++>+++++++++++>++++++++++>++++>+++>++++++++>++++++++++++>+++++++++++>++++++++++>+++++++++++>+++>+<<<<<<<<<<<<-]>-.>--.>---.>++++.>++.>---.>---.>.>.>+.>+++.>.");
