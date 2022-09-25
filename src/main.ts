94186eeeb50b4020e4e0135c7eae37d1ad14264e
Guilherme Marques <marquesguilherme3@gmail.com>
2022-09-25 00:03:08 -0300

    Accept file as argument

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 #! /usr/bin/env node
+import yargs from 'yargs'
 import { BF } from "./compiler";
+import fs from 'fs'
 
-BF.evaluate(
-  "++++++++++[>++++++++>+++++++++++>++++++++++>++++>+++>++++++++>++++++++++++>+++++++++++>++++++++++>+++++++++++>+++>+<<<<<<<<<<<<-]>-.>--.>---.>++++.>++.>---.>---.>.>.>+.>+++.>."
-);
+function main() {
+  const usage = "\nUsage: bfck <file_path>\n";
+  const argv = yargs.usage(usage).help(true).options({}).parseSync();
 
-BF.evaluate(",>++[<----->-]<[----------------------.,----------],");
+  if (argv._.length < 1) {
+    yargs.showHelp()
+  } else {
+    let filepath = argv._[0]
+    try {
+      const data = fs.readFileSync(filepath, 'utf-8')
+      BF.evaluate(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+main()
